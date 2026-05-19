9c5cd3b421a8781fac48b20e71ab790a8fdeb652
Silvio-Ferreira <150188262+Silvio-Ferreira@users.noreply.github.com>
2026-05-19 20:10:01 -0300

    Adicionar arquivos de projeto.

---
+++ ServidorHttp/ServidorHttp.cs
@@ -0,0 +1,28 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+namespace ServidorHttp
+{
+     class ServidorHttp
+    {
+        private TcpListener Controlador {  get; set; }
+        private int Porta { get; set; }
+        private int QtdeRequest { get; set; }
+
+        public ServidorHttp(int porta = 8080)
+        {
+            Porta = porta;
+            try
+            {
+                Controlador = new TcpListener(IPAddress.Parse("127.0.0.1"), Porta);
+                Controlador.Start();
+                Console.WriteLine($"Servidor HTTP iniciado na porta {Porta}");
+                Console.WriteLine($"Para acessar, digite: http://localhost:{Porta}/");
+            }
+            catch ( Exception ex )
+            {
+                Console.WriteLine($"Erro ao iniciar o servidor na porta {Porta}:\n{ex.Message}");
+            }
+        }
+    }
+}
