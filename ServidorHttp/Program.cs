ebc5163eb1f8fa23f7d94255ce3481cb08935b85
Silvio-Ferreira <150188262+Silvio-Ferreira@users.noreply.github.com>
2026-05-21 19:05:35 -0300

    Implementa servidor HTTP assíncrono com processamento de requests
    Adiciona inicialização do servidor HTTP no Main. Refatora ServidorHttp para aceitar conexões de forma assíncrona usando Task. Implementa métodos AguardarRequests e ProcessarRequest para lidar com múltiplas requisições simultâneas e exibir o conteúdo das requisições no console.

--- ServidorHttp/Program.cs
+++ ServidorHttp/Program.cs
@@ -1,1 +1,10 @@
-﻿
+﻿using System;
+using ServidorHttp;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        ServidorHttp.ServidorHttp servidor = new ServidorHttp.ServidorHttp();
+    }
+}
--- ServidorHttp/ServidorHttp.cs
+++ ServidorHttp/ServidorHttp.cs
@@ -1,5 +1,6 @@
 ﻿using System.Net;
 using System.Net.Sockets;
+using System.Text;
 
 namespace ServidorHttp
 {
@@ -18,11 +19,41 @@ namespace ServidorHttp
                 Controlador.Start();
                 Console.WriteLine($"Servidor HTTP iniciado na porta {Porta}");
                 Console.WriteLine($"Para acessar, digite: http://localhost:{Porta}/");
+                Task servidorHttpTask = Task.Run(() => AguardarRequests());
+                servidorHttpTask.GetAwaiter().GetResult();
             }
             catch ( Exception ex )
             {
                 Console.WriteLine($"Erro ao iniciar o servidor na porta {Porta}:\n{ex.Message}");
             }
+        }
+
+        private async Task AguardarRequests()
+        {
+            while(true)
+            {
+                Socket conexao = await Controlador.AcceptSocketAsync();
+                QtdeRequest++;
+                Task task = Task.Run(() => ProcessarRequest(conexao, QtdeRequest));
+            }
+        }
+
+        private void ProcessarRequest(Socket conexao, int numeroRequest)
+        {
+            Console.WriteLine($"Processando request #{numeroRequest}...\n");
+            if(conexao.Connected)
+            {
+                byte[] bytesRequisicao = new byte[1024];
+                conexao.Receive(bytesRequisicao, bytesRequisicao.Length, 0);
+                string textoRequisicao = Encoding.UTF8.GetString(bytesRequisicao)
+                    .Replace((char)0, ' ').Trim();
+                if(textoRequisicao.Length > 0)
+                {
+                    Console.WriteLine($"\n{textoRequisicao}\n");
+                    conexao.Close();
+                }
+            }
+            Console.WriteLine($"\nRequest {numeroRequest} finalizado.");
         }
     }
 }
