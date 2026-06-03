91b829dd622173a6cc4933dbdef0eefd4c582a77
Silvio-Ferreira <150188262+Silvio-Ferreira@users.noreply.github.com>
2026-06-03 09:15:40 -0300

    Servidor HTTP: suporte a arquivos estáticos e MIME
    Implementa resposta com tipos MIME corretos para arquivos estáticos (.css, .png, .ico, etc.), removendo LerArquivo e adicionando lógica para checagem de existência e tipo do arquivo. Retorna 404 para arquivos não encontrados e 415 para tipos não suportados. Atualiza index.html para usar CSS e favicon, adicionando estilos.css e servidor.png.

--- ServidorHttp/ServidorHttp.cs
+++ ServidorHttp/ServidorHttp.cs
@@ -16,6 +16,7 @@ namespace ServidorHttp
         {
             Porta = porta;
             CriarHtmlExemplo();
+            PopularTiposMime();
             try
             {
                 Controlador = new TcpListener(IPAddress.Parse("127.0.0.1"), Porta);
@@ -50,7 +51,7 @@ namespace ServidorHttp
                 conexao.Receive(bytesRequisicao, bytesRequisicao.Length, 0);
                 string textoRequisicao = Encoding.UTF8.GetString(bytesRequisicao)
                     .Replace((char)0, ' ').Trim();
-                if(textoRequisicao.Length > 0)
+                if (textoRequisicao.Length > 0)
                 {
                     Console.WriteLine($"\n{textoRequisicao}\n");
 
@@ -59,17 +60,31 @@ namespace ServidorHttp
                     int iSegundoEspaco = linhas[0].LastIndexOf(' ');
                     string metodoHttp = linhas[0].Substring(0, iPrimeiroEspaco);
                     string recursoBuscado = linhas[0].Substring(
-                        iPrimeiroEspaco + 1, iSegundoEspaco - iPrimeiroEspaco -  1 );
+                        iPrimeiroEspaco + 1, iSegundoEspaco - iPrimeiroEspaco - 1);
                     string versaoHttp = linhas[0].Substring(iSegundoEspaco + 1);
                     iPrimeiroEspaco = linhas[1].IndexOf(' ');
                     string nomeHost = linhas[1].Substring(iPrimeiroEspaco + 1);
 
                     byte[] bytesCabecalho = null;
-                    var bytesConteudo = LerArquivo(recursoBuscado);
-                    if(bytesConteudo.Length > 0)
+                    byte[] bytesConteudo = null;
+                    FileInfo fiArquivo = new FileInfo(ObterCaminhoFisicoArquivo(recursoBuscado));
+                    if (fiArquivo.Exists)
                     {
-                        bytesCabecalho = GerarCabecalho(versaoHttp, "text/html;charset=utf-8", "200",
-                         bytesConteudo.Length);
+                        if (TiposMime.ContainsKey(fiArquivo.Extension.ToLower()))
+                        {
+                            bytesConteudo = File.ReadAllBytes(fiArquivo.FullName);
+                            string tipoMime = TiposMime[fiArquivo.Extension.ToLower()];
+                            bytesCabecalho = GerarCabecalho(versaoHttp, tipoMime, "200",
+                             bytesConteudo.Length);
+                        }
+                        else
+                        {
+                            bytesConteudo = Encoding.UTF8.GetBytes(
+                                "<h1>Erro 415 - Tipo de arquivo não suportado.</h1>");
+                            bytesCabecalho = GerarCabecalho(versaoHttp, "text/html;charset=utf-8",
+                                "415", bytesConteudo.Length);
+                        }
+                        
                     }
                     else
                     {
@@ -78,11 +93,12 @@ namespace ServidorHttp
                         bytesCabecalho = GerarCabecalho(versaoHttp, "text/html;charset=utf-8",
                             "404", bytesConteudo.Length);
                     }
-                    int bytesEnviados = conexao.Send(bytesCabecalho, 
-                        bytesCabecalho.Length, 0);
-                    bytesEnviados += conexao.Send(bytesConteudo, bytesConteudo.Length, 0);
-                    conexao.Close();
-                    Console.WriteLine($"\n{bytesEnviados} bytes enviados em resposta à requisição #{numeroRequest}.");
+                    int bytesEnviados = conexao.Send(bytesCabecalho,
+                            bytesCabecalho.Length, 0);
+                        bytesEnviados += conexao.Send(bytesConteudo, bytesConteudo.Length, 0);
+                        conexao.Close();
+                        Console.WriteLine($"\n{bytesEnviados} bytes enviados em resposta à requisição #{numeroRequest}.");
+                    
                 }
             }
             Console.WriteLine($"\nRequest {numeroRequest} finalizado.");
@@ -108,17 +124,6 @@ namespace ServidorHttp
             html.Append("<h1>Página Estática</h1></body></html>");
             HtmlExemplo = html.ToString();
         }
-
-        public byte[] LerArquivo(string recurso)
-        {
-            string diretorio = "C:\\Projetos\\.Net\\ServidorHttp\\ServidorHttp\\www";
-            string caminhoArquivo = diretorio + recurso.Replace("/", "\\");
-            if (File.Exists(caminhoArquivo))
-            {
-                return File.ReadAllBytes(caminhoArquivo);
-            }
-            else return new byte[0];
-        }
     
         private void PopularTiposMime()
         {
@@ -135,6 +140,12 @@ namespace ServidorHttp
             TiposMime.Add(".ico", "image/ico");
             TiposMime.Add(".woff", "font/woff");
             TiposMime.Add(".woff2", "font/woff2");
+        }
+    
+        public string ObterCaminhoFisicoArquivo(string arquivo)
+        {
+            string caminhoArquivo = "C:\\Projetos\\.Net\\ServidorHttp\\ServidorHttp\\www" + arquivo.Replace("/", "\\");
+            return caminhoArquivo;
         }
     }
 }
